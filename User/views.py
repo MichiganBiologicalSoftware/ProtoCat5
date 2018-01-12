90e254a681939d65b3d92f3fffc9f3f47ed8168d
alex <naichuan@umich.edu>
2018-01-11 20:19:19 -0500

    login page works

--- User/views.py
+++ User/views.py
@@ -6,3 +6,16 @@ from django.shortcuts import render
 def index(request):
     """User index page."""
     return HttpResponse("Testing")
+
+
+def login_user(request):
+	current_profile_info = request.user
+	if (not current_profile_info.is_anonymous()):
+		current_profile_info = ProfileInfo.objects.get(user = current_profile_info)
+	else:
+		current_profile_info = None
+	context = {
+		'title': 'ProtoCat - Login',
+		'current_profile_info': current_profile_info,
+	}
+	return render(request, 'login.html', context)
