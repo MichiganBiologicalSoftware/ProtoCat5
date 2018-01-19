5a92a4faac3c60eb5bba8c120a9c26061255df5d
Drew Ramacher <drewramacher@Drews-MacBook-Air.local>
2018-01-18 20:40:02 -0500

    Added model and delete, get, and postrequest to api_views

--- Chat/models.py
+++ Chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from User.models import UserInfo
 
 # Create your models here.
+class Message(models.Model):
+	sender = models.ForeignKey(UserInfo, related_name="sender_user")
+	recipient = models.ForeignKey(UserInfo, related_name="recip_user")
+	message = models.TextField()
+	timeSent = models.DateTimeField(null = True, auto_now_add = True)
+	deleted = models.BooleanField(default = False)
+	read = models.BooleanField(default = False)
+
+	def __str__(self):
+		return self.sender.username + " to " + self.recipient.username
