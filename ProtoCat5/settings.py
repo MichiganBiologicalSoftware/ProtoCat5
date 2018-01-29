a68f26805dad9f0131f7c1ab4fe33edef414ac14
Ali Turfah <aturfah@umich.edu>
2018-01-29 18:58:00 -0500

    Add ESClient settings

--- ProtoCat5/settings.py
+++ ProtoCat5/settings.py
@@ -128,3 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+ES_CLIENT = Elasticsearch(
+    ['http://localhost:9200/'],
+    connection_class=RequestsHttpConnection
+)
