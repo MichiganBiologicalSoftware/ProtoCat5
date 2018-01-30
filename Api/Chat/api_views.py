bc168398c20705eb500fb79dffc2483ef7725904
Milo Hartsoe <milo.red.duck@gmail.com>
2018-01-29 19:52:24 -0500

    [Chat] finish chat api

--- Api/Chat/api_views.py
+++ Api/Chat/api_views.py
@@ -2,54 +2,29 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
-def inbox_view(request):
-	if request.method == "POST":
-		for key in request.POST:
-			if key[:5] == 'check':
-				id = key[5:]
-				tempMessage = models.Message.objects.get(id=id)
-				tempMessage.deleted = True
-				tempMessage.save()
-
-	if request.user.is_anonymous():
-		return redirect('root_index')
-	else:
-		user = ProfileInfo.objects.get(user = request.user)
-
-	messages = models.Message.objects.filter(recipient=user).filter(deleted=False).order_by('-timeSent')
-
-	for message in messages:
-		message.read = True
-		message.save()
-
-	context = {
-		'title': 'Inbox',
-		'message_list': messages,
-	}
-	if (request.user.is_anonymous()):
-		context['current_profile_info'] = None
-	else:
-		context['current_profile_info'] = request.user.profileinfo
-	return render(request, 'protoChat/inbox.html', context)
-
-def postRequest(request):
-	if request.method == "POST":
-		for key in request.POST:
-			if key[:5] == 'check':
-				id = key[5:]
-				tempMessage = models.Message.objects.get(id=id)
-				tempMessage.deleted = True
-				tempMessage.save()
+from django.http import HttpResponseForbidden
 
 def index(request):
     """Chat API index page."""
-    postRequest(request)
+    if request.method == "POST":
+		post(request)
+    if request.method == "GET":
+		get(request)
+    if request.method == "DELETE":
+		delete(request)
     return HttpResponse("Testing")
 
-def getRequest(request):
+def post(request):
+	for key in request.POST:
+		if key[:5] == 'check':
+			id = key[5:]
+			tempMessage = models.Message.objects.get(id=id)
+			tempMessage.deleted = True
+			tempMessage.save()
+
+def get(request):
 	if request.user.is_anonymous():
-		return redirect('root_index')
+		return HttpResponseForbidden()
 	else:
 		user = ProfileInfo.objects.get(user = request.user)
 
@@ -71,27 +46,10 @@ def getRequest(request):
 
 def delete(request):
 	if request.user.is_anonymous():
-		return redirect('root_index')
+		return HttpResponseForbidden()
 	else:
 		user = ProfileInfo.objects.get(user = request.user)
 	message_id = request.args['id']
-	
 	message = models.Message.objects.filter(recipient=user).filter(id=message_id)
 	message.deleted = True
 	message.save()
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
