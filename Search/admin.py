335de0265d559818f3f16382039ada8b06f02ad0
Ali Turfah <aturfah@umich.edu>
2018-01-29 19:00:41 -0500

    Add to admin to access data that way

--- Search/admin.py
+++ Search/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from Search.models import University, Course, Student 
+
+admin.site.register(University) 
+admin.site.register(Course) 
+admin.site.register(Student)
