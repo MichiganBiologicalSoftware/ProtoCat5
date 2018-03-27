a931cbace2b529bf53867c5ee9961db8d295c720
Ali Turfah <aturfah@umich.edu>
2018-03-26 20:35:48 -0400

    Add preliminary searching

--- Api/Search/api_views.py
+++ Api/Search/api_views.py
@@ -1,8 +1,20 @@
 """Define the API for favorites."""
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from User.models import UserInfo
 from django.shortcuts import render
+from django.core import serializers
 
 def index(request):
     """Favorites API index page."""
-    return HttpResponse("Testing api Search")
+    query = request.GET.get("q", "")
+    
+    users = UserInfo.objects.filter(about__contains=query)
+    match_users = []
+    for user in users:
+        match_users.append(user.user.username)
+
+    return JsonResponse({
+        "query": query,
+        "users": match_users
+    })
