cbdf3f7dbfad2ccdc63c1a08138e60d3cadc7581
Ali Turfah <aturfah@umich.edu>
2018-03-29 20:54:34 -0400

    Add searching multiple word queries

--- Api/Search/api_views.py
+++ Api/Search/api_views.py
@@ -16,11 +16,20 @@ def index(request):
             "users": match_users
         })
 
-    users = UserInfo.objects.filter(about__icontains=query)
-    users = users | UserInfo.objects.filter(user__username__icontains=query)
-    users = users | UserInfo.objects.filter(user__email__icontains=query)
-    users = users | UserInfo.objects.filter(user__first_name__icontains=query)
-    users = users | UserInfo.objects.filter(user__last_name__icontains=query)
+    query_words = query.split()
+
+    users = UserInfo.objects.filter(about__icontains=query_words[0])
+    users = users | UserInfo.objects.filter(user__username__icontains=query_words[0])
+    users = users | UserInfo.objects.filter(user__email__icontains=query_words[0])
+    users = users | UserInfo.objects.filter(user__first_name__icontains=query_words[0])
+    users = users | UserInfo.objects.filter(user__last_name__icontains=query_words[0])
+
+    for word in query_words[1:]:
+        users = users | UserInfo.objects.filter(about__icontains=word)
+        users = users | UserInfo.objects.filter(user__username__icontains=word)
+        users = users | UserInfo.objects.filter(user__email__icontains=word)
+        users = users | UserInfo.objects.filter(user__first_name__icontains=word)
+        users = users | UserInfo.objects.filter(user__last_name__icontains=word)
 
     for user in users:
         match_users.append(user.user.username)
