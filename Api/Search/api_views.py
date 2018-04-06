c8d2fb7e645d79be081528d0fc9c68e6cc26992d
Ali Turfah <aturfah@umich.edu>
2018-04-05 20:04:14 -0400

    Remove email searching

--- Api/Search/api_views.py
+++ Api/Search/api_views.py
@@ -20,14 +20,12 @@ def index(request):
 
     users = UserInfo.objects.filter(about__icontains=query_words[0])
     users = users | UserInfo.objects.filter(user__username__icontains=query_words[0])
-    users = users | UserInfo.objects.filter(user__email__icontains=query_words[0])
     users = users | UserInfo.objects.filter(user__first_name__icontains=query_words[0])
     users = users | UserInfo.objects.filter(user__last_name__icontains=query_words[0])
 
     for word in query_words[1:]:
         users = users | UserInfo.objects.filter(about__icontains=word)
         users = users | UserInfo.objects.filter(user__username__icontains=word)
-        users = users | UserInfo.objects.filter(user__email__icontains=word)
         users = users | UserInfo.objects.filter(user__first_name__icontains=word)
         users = users | UserInfo.objects.filter(user__last_name__icontains=word)
 
