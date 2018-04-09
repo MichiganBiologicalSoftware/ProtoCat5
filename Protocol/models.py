4ab7f268731b660ed82ea3b5782cb66ee3a380a2
Ali Turfah <aturfah@umich.edu>
2018-04-09 19:57:33 -0400

    Preliminary protocol model

--- Protocol/models.py
+++ Protocol/models.py
@@ -1,4 +1,11 @@
 from django.db import models
-
+from User.models import UserInfo
 # Create your models here.
-#Testing branch
+
+class Protocol(models.Model):
+    """Class Docstring"""
+    title = models.TextField()
+    description = models.TextField()
+    materials = models.TextField()
+    author = models.ForeignKey(UserInfo, related_name="protocols")
+	
