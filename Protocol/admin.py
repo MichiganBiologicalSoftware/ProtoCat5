3f05b14c4c516e52483652951385f3d15c06eea9
Ali Turfah <aturfah@umich.edu>
2018-04-09 20:12:12 -0400

    Register steps to admin site

--- Protocol/admin.py
+++ Protocol/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Protocol.models import Protocol
+from Protocol.models import Protocol, ProtocolStep
 
 # Register your models here.
 admin.site.register(Protocol)
+admin.site.register(ProtocolStep)
