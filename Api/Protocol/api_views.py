4dc9beb6c2db86d1c809e61f526d80cecb31e0b2
Ali Turfah <aturfah@umich.edu>
2018-04-12 19:56:39 -0400

    Add preliminary endpoint

--- Api/Protocol/api_views.py
+++ Api/Protocol/api_views.py
@@ -1,8 +1,20 @@
 """Define the API for protocols."""
 
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render
 
-def index(request):
+from Protocol.models import Protocol
+
+def index(request, protocol_id=None):
     """Protocol API index page."""
-    return HttpResponse("Testing")
+    try:
+        protocol = Protocol.objects.get(id=protocol_id)
+    except Exception:
+        # Make a 404 Exception
+        return JsonResponse({"err_msg":"ERROR!"})
+
+    response = {}
+    response["id"] = protocol.id
+
+    return JsonResponse(response)
